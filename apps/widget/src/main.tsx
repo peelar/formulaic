3ee02ad0608eb7f7e864a639fe99c74e7844da29
Adrian Pilarczyk <admin@peelar.dev>
2023-11-03 20:16:17 +0100

    refactor: :recycle: misc

--- apps/widget/src/main.tsx
+++ apps/widget/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const ROOT_ID = "formulaic-widget";
+
+ReactDOM.createRoot(document.getElementById(ROOT_ID)!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
