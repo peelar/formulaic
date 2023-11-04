74890699d2d6c53bf66ac74d59e7180a6a217176
Adrian Pilarczyk <admin@peelar.dev>
2023-11-04 12:08:55 +0100

    refactor: :recycle: customize build output

--- apps/widget/vite.config.ts
+++ apps/widget/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "embed.js",
+        assetFileNames: "embed.css",
+        chunkFileNames: "chunk.js",
+        manualChunks: undefined,
+      },
+    },
+  },
+});
