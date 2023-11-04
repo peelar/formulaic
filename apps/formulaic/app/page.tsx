e649dbeabdda89b92353f9d2c26fa09b09ba1fb5
Adrian Pilarczyk <admin@peelar.dev>
2023-11-04 17:24:34 +0100

    feat: :sparkles: add basic prisma setup

--- apps/formulaic/app/page.tsx
+++ apps/formulaic/app/page.tsx
@@ -1,4 +1,13 @@
+import { db } from "./db";
+
+async function queryUsers() {
+  const allUsers = await db.user.findMany();
+
+  console.log(allUsers);
+}
+
 export default function Page(): JSX.Element {
+  queryUsers();
   return (
     <main>
       <h1>Formulaic</h1>
