8002c1ce42fe7f7919aff37c15b52f5892d40e60
Adrian Pilarczyk <admin@peelar.dev>
2023-11-06 08:33:15 +0100

    refactor: :recycle: add logger to form-service

--- apps/formulaic/app/modules/form/form-service.ts
+++ apps/formulaic/app/modules/form/form-service.ts
@@ -1,5 +1,6 @@
 import { Form } from "@prisma/client";
 import { FormRepository } from "./form-repository";
+import { Logger } from "../../lib/logger";
 
 // todo: move
 type Nullable<T> = T | null | undefined;
@@ -30,6 +31,7 @@ export function isDomainAllowed(
 export class FormService {
   private repository: FormRepository;
   private request: Request;
+  private logger: Logger;
 
   constructor({
     request,
@@ -43,20 +45,26 @@ export class FormService {
   }
 
   async getById(id: Nullable<string>) {
+    this.logger.debug("getById", { id });
+
     if (!id) {
+      this.logger.debug("Form id is either null or undefined");
       return new Response("Missing id", { status: 400 });
     }
 
     const form = await this.repository.getById(id);
 
     if (!form) {
+      this.logger.debug("No form was found for this id");
       return new Response("Not found", { status: 404 });
     }
 
     if (!isDomainAllowed(this.request, form)) {
+      this.logger.debug("Domain not found in form allow list");
       return new Response("Not allowed", { status: 403 });
     }
 
+    this.logger.debug({ form }, "Returning form");
     return new Response(JSON.stringify(form), {
       status: 200,
       headers: {
