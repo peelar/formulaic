ea420b068dd9df03c8b6bb41b5645ecc5a492bdf
Adrian Pilarczyk <admin@peelar.dev>
2023-11-10 17:47:12 +0100

    fix: :bug: pino

--- apps/formulaic/app/lib/logger.ts
+++ apps/formulaic/app/lib/logger.ts
@@ -1,15 +1,19 @@
 import pino from "pino";
+import pretty from "pino-pretty";
+const stream = pretty({
+  colorize: true,
+});
 
 const LOG_LEVEL = process.env.LOG_LEVEL || "debug";
 
-export const logger = pino({
-  level: LOG_LEVEL,
-  prettyPrint: {
-    colorize: true,
-    translateTime: true,
+export const logger = pino(
+  {
+    level: LOG_LEVEL,
   },
-});
+  stream
+);
 
 export type Logger = typeof logger;
 
-export const createLogger = (name: string) => logger.child({ name });
+export const createLogger = ({ name }: { name: string }) =>
+  logger.child({ name });
