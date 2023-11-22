99a8e39a53a707f0999fa628c0a06a4a793f41d9
Adrian Pilarczyk <admin@peelar.dev>
2023-11-22 20:23:17 +0100

    fix: :beers: hopefully fix prisma pooling issue

--- apps/formulaic/prisma.ts
+++ apps/formulaic/prisma.ts
@@ -1,5 +1,16 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-export { Prisma };
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma, Prisma };
